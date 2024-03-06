217ae437c21013b39e80e0425063a4fb10c004d9
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-06 19:51:58 +0300

    Вывод списка товаров первой версии

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../config";
+
+import { Preloader } from "./Preloader";
+import { GoodsList } from "./GoodsList";
+
 function Shop() {
-  return <main className="container content">Shop</main>;
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.featured);
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="container content">
+      {loading ? <Preloader /> : <GoodsList goods={goods} />}
+    </main>
+  );
 }
 
 export { Shop };
